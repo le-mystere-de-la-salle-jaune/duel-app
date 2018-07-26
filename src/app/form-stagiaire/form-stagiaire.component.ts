b33b847793e09bdc55f053787ab2a7a7f1b5b1d6
Aurelie Brizou <brizouaurelie@gmail.com>
2018-07-26 14:01:58 +0200

    form redirection boutton modifier

--- src/app/form-stagiaire/form-stagiaire.component.ts
+++ src/app/form-stagiaire/form-stagiaire.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Stagiaire } from './../domain';
+import { Router } from '@angular/router';
+import { StagiaireService } from './../services/stagiaire.service';
 
 @Component({
   selector: 'app-form-stagiaire',
@@ -10,13 +12,14 @@ export class FormStagiaireComponent implements OnInit {
 
 
  
-  constructor() { }
+  constructor(private router:Router, private stagiaireService:StagiaireService) { }
 stagiaire:Stagiaire = new Stagiaire(1, 'clopin', 'brigitte',  'test@test.fr', 'photo');
   ngOnInit() {
   }
 
   submit() {
     console.log(this.stagiaire);
+    this.router.navigate([this.stagiaireService.currentStagiaireId+'/duels'])
   }
 
 }
