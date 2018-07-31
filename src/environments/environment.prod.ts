1283aa1de4a84ae67be3af92612a29bb6ce96f78
Corentin Torres <torres.corentin@gmail.com>
2018-07-31 17:48:46 +0200

    changement de l'url de l'api en prod (pour taper sur un autre heroku que celui du master)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   
     // ajout d'une URL backend en mode développement
-    backendUrl: 'https://evalme-app.herokuapp.com/api/',
+    backendUrl: 'https://evalme-ctorres.herokuapp.com/',
     stagiairesApi: 'stagiaires/',
     duelsApi: 'duels/',
     resultatsApi: 'resultats-duels/'
